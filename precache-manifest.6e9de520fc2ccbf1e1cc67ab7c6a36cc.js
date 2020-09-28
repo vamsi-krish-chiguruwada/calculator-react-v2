6933a470248ff554c945f2c30f05ba7d6f034551
vamsi-krish-chiguruwada <vamsikrishchiguruwada@gmail.com>
2020-09-28 02:41:18 -0700

    Updates

---
+++ precache-manifest.6e9de520fc2ccbf1e1cc67ab7c6a36cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270f909d01631b84739c58a783eb15f1",
+    "url": "/calculator-react-v2/index.html"
+  },
+  {
+    "revision": "2ab8e7bbb42e64d5f9b6",
+    "url": "/calculator-react-v2/static/css/main.f3e12264.chunk.css"
+  },
+  {
+    "revision": "9ae3daa0af4a4f0e3e79",
+    "url": "/calculator-react-v2/static/js/2.8e5b964d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-react-v2/static/js/2.8e5b964d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab8e7bbb42e64d5f9b6",
+    "url": "/calculator-react-v2/static/js/main.7da7d738.chunk.js"
+  },
+  {
+    "revision": "05a4a97def81dc59aa1c",
+    "url": "/calculator-react-v2/static/js/runtime-main.effef16e.js"
+  }
+]);
